b8b937fa4cd3d69ea88528802a667c59e75a455e
sagarmusabbir <sagarmusabbir@gmail.com>
2024-07-10 01:42:46 +0600

    next-seo and og

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,24 +1,28 @@
 /** @type {import('next-seo').DefaultSeoProps} */
 const defaultSEOConfig = {
-  title: "nomad-flowbiteReact-next-starter",
-  titleTemplate: "%s | nomad-flowbiteReact-next-starter",
-  defaultTitle: "nomad-flowbiteReact-next-starter",
-  description: "Nomad Next.js + FlowbiteReact + TypeScript starter template",
-  canonical: "https://nomad-flowbite-react-next-starter.vercel.app/",
+  title: "Best Platform for sourcing Made in Bangladesh Products",
+  titleTemplate: "%s | Products of Bangladesh",
+  defaultTitle:
+    "Best Platform for sourcing Made in Bangladesh Products | Products of Bangladesh",
+  description:
+    "Products of Bangladesh connects global sellers and brands with top manufacturers and suppliers from bangladesh, specializing in sourcing high-quality products made in Bangladesh for seamless sourcing experience.",
+  canonical: "https://productsofbangladesh.com/",
   openGraph: {
-    url: "https://nomad-flowbite-react-next-starter.vercel.app/",
-    title: "nomad-flowbiteReact-next-starter",
-    description: "Next.js + FlowbiteReact + TypeScript starter template",
+    url: "https://productsofbangladesh.com/",
+    title:
+      "Best Platform for sourcing Made in Bangladesh Products | Products of Bangladesh",
+    description:
+      "Products of Bangladesh connects global sellers and brands with top manufacturers and suppliers from bangladesh, specializing in sourcing high-quality products made in Bangladesh for seamless sourcing experience.",
     type: "website",
     images: [
       {
-        url: "https://yehez-og-image.yehezgun.com/**Nomad**%20nextjs%2BflowbiteReact%2BTypescript%20Starter.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fvercel-triangle-black.svg&images=https%3A%2F%2Fstatic-00.iconduck.com%2Fassets.00%2Fnextjs-icon-512x512-y563b8iq.png&images=https%3A%2F%2Fflowbite.com%2Fimages%2Flogo.svg&images=https%3A%2F%2Fwww.svgrepo.com%2Fshow%2F374146%2Ftypescript-official.svg&widths=50&widths=100&widths=100&widths=100&heights=50&heights=100&heights=100&heights=100",
-        alt: "https://nomad-flowbitenext-starter.vercel.app og-image",
+        url: "https://og.tailgraph.com/og?fontFamily=Roboto&title=Best%20Platform%20for%20sourcing%20Made%20in%20Bangladesh%20Products&titleTailwind=font-extrabold%20text-white%20text-5xl%20text-center%20px-4&titleFontFamily=Inter&text=ProductsofBangladesh.com&textTailwind=text-2xl%20mt-4%20text-center%20text-white&textFontFamily=Inter&logoUrl=https%3A%2F%2Fraw.githubusercontent.com%2Fsagarmusabbir%2Ftb%2Fmain%2Fpublic%2Foldlogo.svg&logoTailwind=text-center%20mx-auto%20w-20%20h-20%20p-4&bgUrl=https%3A%2F%2Fraw.githubusercontent.com%2Fsagarmusabbir%2Ftb%2Fmain%2Fpublic%2Fcover-mint.svg&bgTailwind=bg-white%20bg-opacity-50&footer=ProductsofBangladesh.com&footerTailwind=text-white&containerTailwind=p-4&t=1720553933274&refresh=1",
+        alt: "productsofbangladesh.com og-image",
         width: 800,
         height: 600,
       },
     ],
-    site_name: "nomad-flowbiteReact-next-starter",
+    site_name: "Products of Bangladesh",
   },
   twitter: {
     handle: "@handle",
